7af12886fead6171de0fe37732142042b0cab281
Tee O <tdopress@gmail.com>
2022-08-13 10:23:14 -0400

    implemented fetch to translation API

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
 
+const langCode = {
+    english: 'en',
+    arabic: 'ar',
+    azerbaijani: 'az',
+    chinese: 'zh',
+    czech: 'cs',
+    danish: 'da',
+    dutch: 'nl',
+    esperanto: 'eo',
+    finnish: 'fi',
+    french: 'fr',
+    german: 'de',
+    greek: 'el',
+    hebrew: 'he',
+    hindi: 'hi',
+    hungarian: 'hu',
+    indonesian: 'id',
+    irish: 'ga',
+    italian: 'it',
+    japanese: 'ja',
+    korean: 'ko',
+    persian: 'fa',
+    polish: 'pl',
+    portuguese: 'pt',
+    russian: 'ru',
+    slovak: 'sk',
+    spanish: 'es',
+    swedish: 'sv',
+    turkish: 'tr',
+    ukranian: 'uk'
+}
+
+async function getTranslation() {
+    try {
+        const res = await fetch("https://libretranslate.de/translate", {
+	        method: "POST",
+	        body: JSON.stringify({
+	        	q: "",
+	        	source: "uk",
+	        	target: "es",
+	        	format: "text",
+	        	api_key: ""
+	        }),
+	        headers: { "Content-Type": "application/json" }
+        });
+        let data = await res.json()
+        console.log(data)
+
+    } catch(err) {
+        console.log(err);
+    }
+}

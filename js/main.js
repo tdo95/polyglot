d92b066f183f35e9cb8b4e9b9630c232cdfd3fdb
Tee O <tdopress@gmail.com>
2022-08-14 22:27:19 -0400

    styled interface & added menu transition

--- js/main.js
+++ js/main.js
@@ -31,6 +31,48 @@ const langCode = {
     ukranian: 'uk'
 }
 
+const leftText = document.querySelector('.from-text');
+const rightText = document.querySelector('.translated-text');
+const langMenu = document.querySelector('.lang-menu');
+const langButtons = document.querySelectorAll('.from-lang, .to-lang');
+
+langButtons.forEach(btn => {
+    btn.addEventListener('click', () => toggleLangMenu(btn.id));
+});
+let leftBtnActive, rightBtnActive;
+function toggleLangMenu(id) {
+    if (id === 'left') {
+        if (leftBtnActive) {
+            //close menu
+            //disable left button
+            leftBtnActive = false;
+        } else {
+            leftBtnActive = true;
+            rightBtnActive = false;
+            //show menu
+            leftText.classList.add('hidden');
+            rightText.classList.add('hidden');
+            langMenu.classList.remove('close');
+            langMenu.classList.add('open');
+
+        }
+        
+    } else if (id === 'right') {
+        if (rightBtnActive) {
+            //close menu
+            //disable right button
+            rightBtnActive = false;
+        } else {
+            leftBtnActive = false;
+            rightBtnActive = true;
+            //show menu
+        }
+    }
+    console.log(id, leftBtnActive, rightBtnActive);
+
+}
+
+
 async function getTranslation() {
     try {
         const res = await fetch("https://libretranslate.de/translate", {
